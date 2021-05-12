ee2efcd9a4afc42ca5de774f491aa99a1f1e458a
Nilomiranda <nilomiranda3@gmail.com>
2021-05-12 19:15:19 -0300

    Added following and followers count columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_223334) do
+ActiveRecord::Schema.define(version: 2021_05_12_221426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_05_10_223334) do
     t.string "recovery_password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "following_count", default: 0
+    t.integer "followers_count", default: 0
   end
 
 end
