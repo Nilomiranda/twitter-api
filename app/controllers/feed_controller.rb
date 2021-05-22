374d64149d0c16d9b82a6c56bf89194b97457b88
Nilomiranda <nilomiranda3@gmail.com>
2021-05-21 21:53:51 -0300

    fixed feed endpoint that was returning empty array when not following anyone

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,12 +3,9 @@ class FeedController < ApplicationController
     user_id = @current_user.id
 
     following_ids = Following.where("follower_id = ?", user_id).select(:following_id)
-    puts('following_ids', following_ids.as_json)
-
-    return render json: FeedBlueprint.render([], { root: :feed }) unless following_ids.length != 0
 
     paginated_feed = PaginationService.paginate(
-      Tweet.where('user_id IN(:user_ids)', { user_ids: [*following_ids.map { |following_id| following_id['following_id']  }, @current_user.id] }).order(created_at: :desc),
+      Tweet.where('user_id IN(:user_ids)', { user_ids: [@current_user.id, *following_ids.map { |following_id| following_id['following_id']  }  ] }).order(created_at: :desc),
       params[:page],
       'feed',
     )
