43d690ce88111bef4069e939b52d9e24b172efde
Nilomiranda <nilomiranda3@gmail.com>
2021-05-24 17:33:31 -0300

    added user id param to filter tweets of a certain user

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,10 +2,11 @@ class FeedController < ApplicationController
   def index
     user_id = @current_user.id
 
-    following_ids = Following.where("follower_id = ?", user_id).select(:following_id)
+    following_ids = params[:user_id] ? params[:user_id] : Following.where("follower_id = ?", user_id).select(:following_id)
+    user_ids = params[:user_id] ? params[:user_id] : [@current_user.id, *following_ids.map { |following_id| following_id['following_id']  }  ]
 
     paginated_feed = PaginationService.paginate(
-      Tweet.where('user_id IN(:user_ids)', { user_ids: [@current_user.id, *following_ids.map { |following_id| following_id['following_id']  }  ] }).order(created_at: :desc),
+      Tweet.where('user_id IN(:user_ids)', { user_ids: user_ids }).order(created_at: :desc),
       params[:page],
       'feed',
     )
