d54359c70195665f0916e8833b14754f027063bf
Nilomiranda <nilomiranda3@gmail.com>
2021-06-10 17:15:17 -0300

    user can edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action -> (entity = Tweet) { check_entity_existence entity }, only: [:create]
-  before_action -> (entity = Comment) { check_entity_existence entity }, only: [:delete, :update]
-  before_action -> (for_user = false, entity = Comment.find_by(id: params[:id])) { check_ownership(for_user, entity) }, only: [:delete, :update]
+  before_action -> (entity = Comment) { check_entity_existence entity }, only: [:delete, :edit]
+  before_action -> (for_user = false, entity = Comment.find_by(id: params[:id])) { check_ownership(for_user, entity) }, only: [:delete, :edit]
 
   def create
     comment = Comment.new(comment_params)
@@ -20,6 +20,19 @@ class CommentsController < ApplicationController
   def delete
     comment = Comment.find_by(id: params[:id])
     comment.destroy
+  end
+
+  def edit
+    comment = Comment.find_by(id: params[:id])
+    comment.content = comment_params[:content]
+
+    if comment.save
+      render json: CommentBlueprint.render(comment, { root: :comment })
+    else
+      render :json => {
+        errors: comment.errors
+      }, status: 400
+    end
   end
 
   private
